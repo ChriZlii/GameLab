eca77359143e75b6907f14315727e332a400b374
ChriZlii <57497515+ChriZlii@users.noreply.github.com>
2019-11-17 15:37:20 +0100

    Rework codestructure for adding Multilpayer easyer

---
+++ Assets/Scripts/GunData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunData : MonoBehaviour
+{
+
+    public GameObject WeabonMuzzle;
+
+    [Header("Weapon Data")]
+    public float Damage = 15f;
+    public float Distance = 100f;
+    public float FireRate = 10f;
+    public float NextTimeToFire = 0;
+}
---
+++ Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(WeaponholderScript))]
+public class ShootingScript : MonoBehaviour
+{
+    //Public Variables
+
+    // Private Variables
+    private InputController controls = null;
+    private WeaponholderScript weaponholder = null;
+    private GunData gunData = null;
+
+
+
+    private void Awake()
+    {
+        weaponholder = GetComponent<WeaponholderScript>();
+        controls = new InputController();
+    }
+    private void OnEnable() => controls.Player.Enable();
+    private void OnDisable() => controls.Player.Disable();
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        gunData = weaponholder.SelectedWeapon.GetComponent<GunData>();
+
+        if (controls.Player.Shoot.ReadValue<float>() == 1 && Time.time >= gunData.NextTimeToFire)
+        {
+            gunData.NextTimeToFire = Time.time + 1f / gunData.FireRate;
+            Shoot();
+        }
+    }
+
+
+
+
+
+    void Shoot()
+    {
+        RaycastHit rayHit;
+        if (Physics.Raycast(gunData.WeabonMuzzle.transform.position, gunData.WeabonMuzzle.transform.forward, out rayHit, gunData.Distance))
+        {
+            // hit something
+            Debug.Log(rayHit.collider.name);
+        }
+    }
+}
+
+
+
+
