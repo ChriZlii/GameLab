2f7f27798ef0c05fd09935fc6ac64e5bfebb5ee1
ChriZlii <57497515+ChriZlii@users.noreply.github.com>
2020-02-27 17:41:14 +0100

    Removed singelton in Itemmanager

--- Assets/Scripts/ItemManager/ItemManager.cs
+++ Assets/Scripts/ItemManager/ItemManager.cs
@@ -10,22 +10,17 @@ public class ItemManager : NetworkBehaviour
     //Public
     public static List<ItemSpawnPoint> ItemSpawnPoints = new List<ItemSpawnPoint>();
 
-    public static ItemManager singleton;
-
 
     //Private
 
 
     // only the server is permitted to spawn Items!
     public void OnServerStart()
-    {
-        InitializeSingleton();
-        
+    {        
         foreach (ItemSpawnPoint spawnpoint in ItemManager.ItemSpawnPoints)
         {
             spawnpoint.SpawnRandomItem();
         }
-
     }
 
     
@@ -46,38 +41,6 @@ public class ItemManager : NetworkBehaviour
     }
 
 
-
-
-
-    // maybe quit conection to networkmanager
-    void InitializeSingleton()
-    {
-        if (singleton != null && singleton == this)
-        {
-            return;
-        }
-
-        // do this early
-        LogFilter.Debug = NetworkManager.singleton.showDebugMessages;
-
-        if (NetworkManager.singleton.dontDestroyOnLoad)
-        {
-            if (singleton != null)
-            {
-                Debug.LogWarning("Multiple ItemManagers detected in the scene. Only one ItemManagers can exist at a time. The duplicate ItemManagers will be destroyed.");
-                Destroy(gameObject);
-                return;
-            }
-            if (LogFilter.Debug) Debug.Log("ItemManager created singleton (DontDestroyOnLoad)");
-            singleton = this;
-            if (Application.isPlaying) DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            if (LogFilter.Debug) Debug.Log("ItemManager created singleton (ForScene)");
-            singleton = this;
-        }
-    }
 
 
 
