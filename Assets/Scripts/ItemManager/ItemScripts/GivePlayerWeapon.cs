9a3ba88e2d915438610275e77c6eec035fd48eb7
ChriZlii <57497515+ChriZlii@users.noreply.github.com>
2020-03-28 15:51:06 +0100

    changed structure of GivePlayerWeapon script

--- Assets/Scripts/ItemManager/ItemScripts/GivePlayerWeapon.cs
+++ Assets/Scripts/ItemManager/ItemScripts/GivePlayerWeapon.cs
@@ -14,29 +14,33 @@ public class GivePlayerWeapon : NetworkBehaviour
 
 
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider playerCollider)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (playerCollider.gameObject.CompareTag("Player"))
         {
-            GameObject player = other.gameObject;
+            GameObject player = playerCollider.gameObject;
             NetworkIdentity identity = player.GetComponent<NetworkIdentity>();
 
             if (identity.isLocalPlayer)
             {
-                List<object> messageData = new List<object>
-                {
-                    weaponType,         // type of weapon
-                    true                // true = give / false = take ... weapon
-                };
-                if (destroyOnUse) messageData.Add(netId); // netid of the Item
-
-                player.SendMessage("Msg_GiveWeapon", messageData, SendMessageOptions.RequireReceiver);
-
+                // If player is in activation radius
+                Activate(player);
             }
         }
     }
 
 
+    private void Activate(GameObject player)
+    {
+        List<object> messageData = new List<object>
+                {
+                    weaponType,         // type of weapon
+                    true                // true = give / false = take ... weapon
+                };
+        if (destroyOnUse) messageData.Add(netId); // netid of the Item
+
+        player.SendMessage("Msg_GiveWeapon", messageData, SendMessageOptions.RequireReceiver);
+    }
 
 
 
